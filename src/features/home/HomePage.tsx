f7aeb8697100f56d9259e6fd9824db83ee994e3c
mrrobot331 <ivan126ns@gmail.com>
2023-11-29 15:05:32 +0100

    1

---
+++ src/features/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from "@mui/material";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Typography variant="h2">
+        HomePage
+      </Typography>
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
+import { StoreProvider } from './app/context/StoreContext';
+import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore';
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+      <StoreProvider>
+        <Provider store={store}>
+          <RouterProvider router={router} />
+          </Provider>
+      </StoreProvider>
+      
+   
   </React.StrictMode>
 );
 
